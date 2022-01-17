947597241b08d6a18d714eddc3b646c2601a7521
Justin Peisker <justinpeisker@gmail.com>
2022-01-17 08:15:25 -0800

    wip

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,60 @@
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  // DO YOUR MAGIC 
+  try{
+    res.json('get accounts')
+  }
+  catch(err) {
+    next({status: 422, message: 'uh oh!'})
+  }
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    res.json('get id accounts')
+  }
+  catch(err) {
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    res.json('post accounts')
+  }
+  catch(err) {
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    res.json('post id accounts')
+  }
+  catch(err) {
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    res.json('delete accounts')
+  }
+  catch(err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status|| 500).json({
+    message: err.message,
+  })
 })
 
 module.exports = router;
